a874c2e634f6ba36f0db816331b79fe453d34f97
Dan Nasman <dan.nasman@gmail.com>
2023-01-22 00:47:11 +0200

    tidy up main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod inter;
 mod lexer;
 mod parser;
+use std::time::Instant;
 use std::env;
 use std::error::Error;
 use std::fs;
@@ -11,14 +12,20 @@ fn main() -> Result<(), Box<dyn Error>> {
     if args.len() > 1 {
         filename = args[1].clone();
     }
+
     let lexer = lexer::Lexer::new();
     let mut parser = parser::Parser::new(lexer);
-    //let input = String::from("{\n\tx = 2;\n\t{\n\t\ty = x;\n\t}\n\t{\n\t\tz = x;\n\t}\n}");
-    //let input = String::from("{\n\tx = -1;\n\twhile (!(x > 5)) {\n\t\twhile(x < 3) {\n\t\t\tx = x + 1;\n\t\t}\n\t\tx = x * 2;\n\t}\n}");
-    //let input = String::from("{\n\tx = 1;\n\twhile (true) {\n\t\tif(x < 3) {\n\t\t\tx = x + 1;\n\t\t} else {\n\t\t\tbreak;\n\t\t}\n\t}\n\ty=2;\n}");
+
     let input: String = fs::read_to_string(filename)?.parse()?;
     println!("{}", input);
     println!("----------compiling----------");
+
+    let now = Instant::now();
+    
     parser.program(&input);
+    
+    let elapsed = now.elapsed();
+    
+    println!("Code compiled in {:?}", elapsed);
     Ok(())
 }
