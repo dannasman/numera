41d2ae2cf3e9b22918472304e741c5d529e6cb16
Dan Nasman <dan.nasman@gmail.com>
2025-01-11 17:06:27 +0200

    update command line args

--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,39 @@ mod tac;
 mod tokens;
 
 fn main() {
-    let input = env::args().nth(1);
-    let reader: Box<dyn BufRead> = match input {
+    let mut args: Vec<String> = env::args().collect();
+    let reader: Box<dyn BufRead> = match args.pop() {
         None => Box::new(BufReader::new(io::stdin())),
         Some(filename) => Box::new(BufReader::new(fs::File::open(filename).unwrap())),
     };
+
+    let mut print_ir = false;
+
+    while let Some(s) = args.pop() {
+        if s == "--ir" {
+            print_ir = true;
+        }
+    }
+
     let lexer = lexer::Lexer::new(reader);
     let mut parser = parser::Parser::new(lexer).expect("Creating parser");
 
-    //println!("================TAC================");
-
     let mut ir = tac::TACIr::new();
     parser.program(&mut ir).expect("Parsing program");
-    //println!("{}", ir);
+
+    if print_ir {
+        println!("================Start of TAC IR================");
+        println!("{}", ir);
+        println!("================End of TAC IR================");
+    }
 
     //println!("================asm================");
 
     let mut codegen = codegen::CodeGenerator::new();
     let mut str = String::new();
     codegen.program(&mut ir, &mut str).expect("Generating code");
-    println!("{}", str);
+
+    if !print_ir {
+        println!("{}", str);
+    }
 }
